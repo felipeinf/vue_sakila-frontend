c6c0b9603ddec214e907e97af291b54ca01149d1
Felipe mac <fconcha.almeida@gmail.com>
2020-01-08 17:49:49 -0300

    refactor(app): set BaseUrl of production

--- src/axios.config.js
+++ src/axios.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  baseURL: 'http://localhost:3000/',//'http://ec2-3-84-3-123.compute-1.amazonaws.com:3000/',
+  baseURL: 'http://ec2-3-84-3-123.compute-1.amazonaws.com:3000/',
   headers: {
     'Content-Type': 'application/json',
   }
